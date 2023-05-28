a9301ee7747fc4ce4df817f4f347b14bfcf05e4c
Srinivasa Reddy Duggempudi <70263151+SrinivasaDuggempudi@users.noreply.github.com>
2023-05-28 15:18:30 -0700

    Add files via upload

---
+++ utils/config.js
@@ -0,0 +1,14 @@
+/**
+*  Database connection details
+*/
+
+var config = {
+    "user": "sa",
+    "password": "XXXXXX",
+    "server": "xx.xx.xx.xx",
+    "database": "BFEnterprise",
+    "driver": "msnodesqlv8",
+    "port": 51423
+  };
+
+module.exports = config;
